52118f235ea7a1c4c0258bf2217b633d8349d2f6
Pwidmann <philipwidmann1@gmail.com>
2020-07-07 07:57:20 +0200

    AABB Collision added

---
+++ Assets/PhysicsManager.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// https://developer.mozilla.org/en-US/docs/Games/Techniques/3D_collision_detection
+
+public class PhysicsManager : MonoBehaviour
+{
+    public Sphere ball;
+
+    public List<Box> boxList = new List<Box>();
+
+    public GameObject[] foundBoxes;
+
+    void Start()
+    {
+        foundBoxes = GameObject.FindGameObjectsWithTag("Box");
+
+        foreach (GameObject box in foundBoxes)
+        {
+            boxList.Add(box.GetComponent<Box>());
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        CheckForCollisions();
+
+        // Add gravity
+        if(!isSphereIntersectingAABB(ball, boxList[0].bounds))
+            ball.GetComponent<Sphere>().AddGravity();
+
+        // Move ball
+        ball.GetComponent<Sphere>().Move();
+    }
+
+    void CheckForCollisions()
+    {
+
+        if (isSphereIntersectingAABB(ball, boxList[0].bounds))
+        {
+            ball.ChangeMovementOnCollision(-Vector3.up, ball.bounciness * boxList[0].bounciness);
+            ball.isColliding = true;
+        }
+        else
+        {
+            ball.isColliding = false;
+        }
+    }
+
+    public bool isPointIntersectingAABB(Vector3 point, Bounds box)
+    { 
+        return (point.x >= box.min.x && point.x <= box.max.x) &&
+         (point.y >= box.min.y && point.y <= box.max.y) &&
+         (point.z >= box.min.z && point.z <= box.max.z);
+    }
+
+    public bool isSphereIntersectingAABB(Sphere sphere, Bounds box)
+    {
+        // get box closest point to sphere center by clamping
+        var x = Math.Max(box.min.x, Math.Min(sphere.position.x, box.max.x));
+        var y = Math.Max(box.min.y, Math.Min(sphere.position.y, box.max.y));
+        var z = Math.Max(box.min.z, Math.Min(sphere.position.z, box.max.z));
+
+        // this is the same as isPointInsideSphere
+        var distance = Math.Sqrt((x - sphere.position.x) * (x - sphere.position.x) +
+                                 (y - sphere.position.y) * (y - sphere.position.y) +
+                                 (z - sphere.position.z) * (z - sphere.position.z));
+
+        return distance <= sphere.radius;
+    }
+}
---
+++ Assets/Sphere.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere : MonoBehaviour
+{
+    public Vector3 position;
+    public float radius;
+    public float bounciness = 0.7f;
+    public float gravity = -9.81f;
+
+
+   
+
+    public Vector3 movement;
+
+    public float movementMagnitude;
+
+    public bool isColliding = false;
+
+    Renderer renderer;
+
+    void Start()
+    {
+        position = transform.position;
+        renderer = GetComponent<Renderer>();
+        radius = renderer.bounds.extents.x;
+    }
+
+    
+    void Update()
+    {
+        // Update sphere world position
+        position = transform.position;
+
+        movementMagnitude = movement.magnitude;
+    }
+
+    public void AddMovement(Vector3 additionalMovement)
+    {
+            movement += additionalMovement;
+    }
+
+    public void AddGravity()
+    {
+        if (!isColliding && movement.magnitude >= 0.01f)
+            movement.y += gravity * Time.fixedDeltaTime;
+    }
+
+    public void Move()
+    {
+        if (movement.magnitude >= 0.1f)
+            transform.Translate(movement * Time.fixedDeltaTime);
+        else
+            movement = Vector3.zero;
+    }
+
+    public void ChangeMovementOnCollision(Vector3 normal, float bounciness)
+    {
+        if (!isColliding)
+        {
+            movement = Quaternion.AngleAxis(180, normal) * movement;
+            movement *= -bounciness; // das - dreht den Vektor weg von der Kollision
+        }
+    }
+}
